eb5691c88e2382f947807850ac25a0ba171880b3
Douglas Meyer <Douglas.Meyer@mail.com>
2014-06-16 06:05:55 -0500

    Added another example, and made example-previews a bit nicer.

--- src/examples/6a1b0eab-a13a-4582-979b-629f191e6daa/src/index.js
+++ src/examples/6a1b0eab-a13a-4582-979b-629f191e6daa/src/index.js
@@ -1,4 +1,27 @@
 angular.module('BuildJS', ['ng'])
 .controller('ExamplesCtrl', function($scope, ExampleData){
-  $scope.examples = ExampleData;
+  $scope.examples = [];
+  for (var id in ExampleData){
+    $scope.examples.push(ExampleData[id]);
+  }
+})
+.filter('hexify', function(){
+  var max = 256,
+      min = max * 1/2,
+      diff = max-min,
+      bound = Math.pow(diff, 3);
+
+  return function(str){
+    var hash = 0, i, chr, len;
+    if (str.length == 0) return hash;
+    for (i = 0, len = str.length; i < len; i++) {
+      chr   = str.charCodeAt(i);
+      hash  = ((hash << 5) - hash) + chr;
+    }
+    hash = (hash % bound + bound) % bound;
+    var r = Math.floor(hash % diff + min),
+        g = Math.floor((hash / diff) % diff + min),
+        b = Math.floor((hash / diff / diff) % diff + min);
+    return 'rgb('+r+','+g+','+b+');';
+  };
 });
